e6e9c52d8571a0f49de7576130e01fefd3e9bfba
aman <90039229+amankhan789@users.noreply.github.com>
2021-11-18 18:36:41 +0530

    react  covid sideabr and navbar
    sidebar

--- navbar.js
+++ navbar.js
@@ -1,17 +1,99 @@
-import React from "react";
+import React, { useState, useEffect } from 'react'
+
+
+import Sidebar from './sidebar'
 import { Link, NavLink } from "react-router-dom";
-import Reactlogo from'../../assets/reactlogo.png';
+import Reactlogo from '../assets/reactlogo.png';
+
 const Navbar = () => {
-  return (
 
-    <nav class="nav_login">    
-        <img  class="nav_log" src={Reactlogo}/>
+    const [data, setData] = useState([]);
+    const getCovidData = async () => {
+        
+        // return new Promise((resolve,reject) => {
+        // const result =  fetch('https://data.covid19india.org/data.json');
+        
+        //     if(result){
+        //         const actualdata =  result.json();
+                
+        //         return resolve(setData(actualdata.statewise));
+        //     }else{
+        //         return reject({status : 'failed',message : 'Data not found'});
+        //     }
+        // })
 
-        <Link to="/"   className="  btn btn-warning">Logout</Link>
+        try {
+            const res = await fetch('https://data.covid19india.org/data.json');
+            const actualdata = await res.json();
+            setData(actualdata.statewise);
+            console.log(actualdata)
+        } catch (err) {
+            console.log(err)
+        }
+    }
 
-    </nav>
-      
-  );
-};
 
-export default Navbar;
+
+
+    useEffect(() => {
+        getCovidData()
+    }, [])
+
+    const [staus, setStatus] = useState(true)
+
+    return (
+        <>
+
+
+            <nav class="nav_login">
+                <img class="nav_log img-fluid" src={Reactlogo} />
+
+                <Link to="/" className="Btn">Logout</Link>
+
+            </nav>
+
+            <section>
+                <div className="container-fluid ps-0">
+                    <div className="row">
+                        <div className="col-md-2">
+                            {
+                                staus ? <Sidebar /> : null
+                            }
+                            <div className="Toggle-sidebar">
+                                <button className="toggle_btn" onClick={() => setStatus(!staus)}> </button>
+                                <span class="glyphicon-class"></span>
+                            </div>
+                        </div>
+
+                        <div className="col-md-10 flex_box">
+
+                            {
+                                data.map(value => {
+                                    return (
+                                        <>
+                                            <div className="flexbox_container ">
+                                                <div className="flexbox_item">
+                                                    Confirmed:
+                                                    <p className="card_data">{value.confirmed} </p>
+                                                </div>
+                                                <div className="flexbox_item">
+                                                    Active:
+                                                    <p className="card_data">{value.active} </p>
+                                                </div>
+                                            </div>
+                                        </>
+                                    )
+                                })
+
+                            }
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+        </>
+    )
+}
+
+export default Navbar
--- sidebar.js
+++ sidebar.js
@@ -1,26 +1,32 @@
-import React from "react";
+import React from 'react'
 import { Link, NavLink } from "react-router-dom";
-import { IconName } from "react-icons/";
+//import { IconName } from "react-icons/";
 
 
 const Sidebar = () => {
-  return (
-    <div class="sidenav">
-      <div className="side_input">
-      <Link to="/home"> <i class="fa fa-folder">        </i>Home</Link>
-      <Link to="/home"> <i class="fa fa-folder">      </i>Shipping</Link>
-      <Link to="/home"> <i class="fa fa-folder">      </i>Order</Link>
-      <Link to="/Update/:id"><i class="fa fa-folder"> </i>Update profile</Link>
-      <Link to="/Addstrip"><i class="fa fa-folder">   </i>Add payment method</Link>
-      <Link to="/Checkout"><i class="fa fa-folder">   </i>Setting</Link>
-      </div>
-      <div>
+    return (
+        <div className="sidenav" id="mySidebar">
+            <div className="side_input">
+                <ul>
+                    <li>
+                        <Link to="/home"> <i class="fa fa-folder">        </i>Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/home"> <i class="fa fa-folder">        </i>Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/home"> <i class="fa fa-folder">        </i>Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/home"> <i class="fa fa-folder">        </i>Home</Link>
+                    </li>
 
-      </div>
 
-    </div>
-  );
-};
+                </ul>
+            </div>
 
-export default Sidebar;
+        </div>
+    )
+}
 
+export default Sidebar
